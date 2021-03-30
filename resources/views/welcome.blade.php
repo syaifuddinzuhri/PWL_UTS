9e09be15ae83134e2ae2b182b74df2506997abb1
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-30 23:26:23 +0700

    Edit view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,19 @@
 @section('title', 'Beranda')
 
 @section('content')
-    <div class="row my-5">
+    <section>
         <div class="container">
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">UTS Pemrograman Web Lanjut</h1>
-                    <hr>
-                    <p class="lead">Mochammad Syaifuddin Zuhri | TI-2C | 1941720013</p>
+            <div class="row my-5">
+                <div class="col-12">
+                    <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                            <h1 class="display-4">UTS Pemrograman Web Lanjut</h1>
+                            <hr>
+                            <p class="lead">Mochammad Syaifuddin Zuhri | TI-2C | 1941720013</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
