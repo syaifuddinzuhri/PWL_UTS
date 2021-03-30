5df1eb7635bdbd683872c662dbcb667e9fa8d2fb
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-30 21:06:41 +0700

    Add field in product and categorie migration

--- database/migrations/2021_03_30_135327_create_products_table.php
+++ database/migrations/2021_03_30_135327_create_products_table.php
@@ -15,7 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255)->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('qty')->nullable();
+            $table->unsignedBigInteger('categorie_id');
             $table->timestamps();
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
