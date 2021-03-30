b5adeb4a504ef92d28654de1e775a77b8e3103ff
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-31 01:41:16 +0700

    change faker type of price in product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -25,7 +25,7 @@ class ProductFactory extends Factory
         return [
             'code_product' => $code,
             'name' => $this->faker->words(5, true),
-            'price' => $this->faker->randomFloat(2, 10, 100),
+            'price' => $this->faker->numberBetween($min = 1000, $max = 200000),
             'qty' => $this->faker->numberBetween($min = 0, $max = 200),
             'categorie_id' => $this->faker->numberBetween($min = 1, $max = 4),
         ];
