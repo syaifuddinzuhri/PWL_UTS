81005589623c05df09af4058cea0426d86300f3a
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-03-30 22:21:44 +0700

    Add query get all categories in index of CategorieController and create view blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::with('categorie')->paginate(10);
+        return view('product.index', compact('products'));
     }
 
     /**
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,15 +3,79 @@
 @section('title', 'Products')
 
 @section('content')
-    <div class="row my-5">
+    <section>
         <div class="container">
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">UTS Pemrograman Web Lanjut</h1>
-                    <hr>
-                    <p class="lead">Mochammad Syaifuddin Zuhri | TI-2C | 1941720013</p>
+            <div class="row mt-5">
+                <div class="col-lg-8">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">All Products</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="col-lg-4 text-right">
+                    <a href="{{ route('product.create') }}" class="btn btn-success"> Create new
+                        product</a>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+    <section class="mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
+                </div>
+                <div class="col-12">
+                    <div class="card shadow">
+                        <div class="card-header">
+                            <h5 class="card-title font-weight-bold m-0">Data products</h5>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Categorie</th>
+                                    <th>Price</th>
+                                    <th>Qty</th>
+                                    <th width="280px">Action</th>
+                                </tr>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ ($products->currentpage() - 1) * $products->perpage() + $loop->index + 1 }}
+                                        </td>
+                                        <td>{{ $product->name }}</td>
+                                        <td>{{ $product->categorie->name }}</td>
+                                        <td>{{ $product->price }}</td>
+                                        <td>{{ $product->qty }}</td>
+                                        <td>
+                                            <form action="{{ route('product.destroy', $product->id) }}" method="POST">
+                                                <a class="btn btn-info"
+                                                    href="{{ route('product.show', $product->id) }}">Show</a>
+                                                <a class="btn btn-primary"
+                                                    href="{{ route('product.edit', $product->id) }}">Edit</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                        <div class="card-footer">
+                            {{ $products->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
